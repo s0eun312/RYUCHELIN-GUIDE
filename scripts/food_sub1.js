6acdcc689bf2a715c4f03829332019cfb1065dec
s0eun312 <159696730+s0eun312@users.noreply.github.com>
2024-06-07 23:17:14 +0900

    Delete scripts directory

--- scripts/food_sub1.js
+++
@@ -1,6 +0,0 @@
-// back 버튼
-let back = document.querySelector('.back');
-
-back.addEventListener('click', () => {
-    window.location.href = "food_index.html";
-});
--- scripts/recommend.js
+++
@@ -1,6 +0,0 @@
-// 폼 제출 alert 창
-let submit = document.querySelector('.submit');
-
-submit.addEventListener('click', () => {
-    alert("감사합니다! 맛있는 하루 보내세요 :)")
-});
